3d7a2a031563e9eaf4f1dd17a2b9376009f11f99
Jean Finck <jeanfinck29@gmail.com>
2021-09-28 15:02:57 +0200

    Jour 2.2

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,3 +1,18 @@
+let ecran = document.getElementById('ecran')
+
 function appuitouche(num) {
-document.getElementById("ecran").innerHTML += num
+    ecran.innerHTML += num
+}
+function appuisymbole(sym) {
+    let pExiste = ecran.innerHTML.indexOf('+')
+    let mExiste = ecran.innerHTML.indexOf('-')
+    let fExiste = ecran.innerHTML.indexOf('*')
+    let dExiste = ecran.innerHTML.indexOf('/')
+
+    if (pExiste == -1 && mExiste == -1 && fExiste == -1 && dExiste == -1) {
+        ecran.innerHTML += sym
+    }
+}
+function appuiclear() {
+    ecran.innerHTML = ""
 }
