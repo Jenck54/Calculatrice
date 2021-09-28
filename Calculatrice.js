3c5812a65f422a8b6d31a5006c807329d9cb23bc
Jean Finck <jeanfinck29@gmail.com>
2021-09-28 16:10:39 +0200

    Jour 2.3

--- Calculatrice.js
+++ Calculatrice.js
@@ -16,3 +16,32 @@ function appuisymbole(sym) {
 function appuiclear() {
     ecran.innerHTML = ""
 }
+function egal() {
+    let pExiste = ecran.innerHTML.indexOf('+')
+    let mExiste = ecran.innerHTML.indexOf('-')
+    let fExiste = ecran.innerHTML.indexOf('*')
+    let dExiste = ecran.innerHTML.indexOf('/')
+
+    if (pExiste > -1) {
+        let nombres = ecran.innerHTML.split('+')
+        ecran.innerHTML = (+nombres[0]) + (+nombres[1])
+    }
+    else {
+        if (mExiste > -1) {
+            let nombres = ecran.innerHTML.split('-')
+            ecran.innerHTML = (+nombres[0]) - (+nombres[1])
+        }
+        else {
+            if (fExiste > -1) {
+                let nombres = ecran.innerHTML.split('*')
+                ecran.innerHTML = (+nombres[0]) * (+nombres[1])
+            }
+            else {
+                if (dExiste > -1) {
+                    let nombres = ecran.innerHTML.split('/')
+                    ecran.innerHTML = (+nombres[0]) / (+nombres[1])
+                }
+            }
+        }
+    }
+}
